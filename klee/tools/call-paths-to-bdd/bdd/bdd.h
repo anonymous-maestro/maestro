dab5efd9514d3cdd198c12640193f59b9f75cdb1
Anonymous Maestro Submission <asubmission21@gmail.com>
2023-05-04 16:31:22 +0100

    submit

---
+++ klee/tools/call-paths-to-bdd/bdd/bdd.h
@@ -0,0 +1,103 @@
+#pragma once
+
+#include "nodes/node.h"
+#include "symbol-factory.h"
+
+#include "klee-util.h"
+
+namespace BDD {
+
+class BDDVisitor;
+
+class BDD {
+private:
+  node_id_t id;
+  unsigned total_call_paths;
+
+  BDDNode_ptr nf_init;
+  BDDNode_ptr nf_process;
+
+  static std::vector<std::string> skip_conditions_with_symbol;
+
+  static constexpr char INIT_CONTEXT_MARKER[] = "start_time";
+  static constexpr char MAGIC_SIGNATURE[] = "===== VIGOR_BDD_SIG =====";
+
+public:
+  BDD(std::vector<call_path_t *> call_paths)
+      : id(0), total_call_paths(call_paths.size()) {
+    kutil::solver_toolbox.build();
+
+    call_paths_t cp(call_paths);
+    auto root = populate(cp);
+
+    nf_init = populate_init(root);
+    nf_process = populate_process(root);
+
+    rename_symbols();
+  }
+
+  BDD(const BDD &bdd)
+      : id(bdd.id), total_call_paths(bdd.total_call_paths),
+        nf_init(bdd.nf_init), nf_process(bdd.nf_process) {}
+
+  BDD(const std::string &file_path) : id(0), total_call_paths(0) {
+    kutil::solver_toolbox.build();
+    deserialize(file_path);
+  }
+
+  // For deserialization
+  BDD() : id(0), total_call_paths(0) { kutil::solver_toolbox.build(); }
+
+  BDD &operator=(const BDD &) = default;
+
+  node_id_t get_id() const { return id; }
+
+  void set_id(node_id_t _id) {
+    assert(_id >= id);
+    id = _id;
+  }
+
+  unsigned get_total_call_paths() const { return total_call_paths; }
+  unsigned get_number_of_nodes(BDDNode_ptr root) const;
+
+  BDDNode_ptr get_init() const { return nf_init; }
+  BDDNode_ptr get_process() const { return nf_process; }
+  BDDNode_ptr get_node_by_id(node_id_t _id) const;
+
+  BDD clone() const;
+
+  void visit(BDDVisitor &visitor) const;
+  void serialize(std::string file_path) const;
+
+  call_t get_successful_call(std::vector<call_path_t *> call_paths) const;
+
+  BDDNode_ptr
+  populate(call_paths_t call_paths,
+           klee::ConstraintManager accumulated = klee::ConstraintManager());
+
+  static std::string get_fname(const Node *node);
+  static bool is_skip_function(const Node *node);
+  static bool is_skip_condition(const Node *node);
+
+  BDDNode_ptr populate_init(const BDDNode_ptr &root);
+  BDDNode_ptr populate_process(const BDDNode_ptr &root, bool store = false);
+
+  void add_node(call_t call);
+  void dump(int lvl, BDDNode_ptr node) const;
+
+  void rename_symbols() {
+    SymbolFactory factory;
+
+    rename_symbols(nf_init, factory);
+    rename_symbols(nf_process, factory);
+  }
+
+  void rename_symbols(BDDNode_ptr node, SymbolFactory &factory);
+  void deserialize(const std::string &file_path);
+
+public:
+  friend class CallPathsGroup;
+  friend class Call;
+};
+
+} // namespace BDD
---
+++ klee/tools/call-paths-to-bdd/bdd/nodes/node.h
@@ -0,0 +1,129 @@
+#pragma once
+
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+
+#include "load-call-paths.h"
+#include "symbol.h"
+
+namespace BDD {
+
+class Node;
+class BDDVisitor;
+
+typedef std::shared_ptr<Node> BDDNode_ptr;
+typedef uint64_t node_id_t;
+
+class Node {
+public:
+  enum NodeType { BRANCH, CALL, RETURN_INIT, RETURN_PROCESS, RETURN_RAW };
+
+protected:
+  node_id_t id;
+  NodeType type;
+
+  BDDNode_ptr next;
+  BDDNode_ptr prev;
+
+  klee::ConstraintManager constraints;
+
+public:
+  Node(node_id_t _id, NodeType _type, klee::ConstraintManager _constraints)
+      : id(_id), type(_type), next(nullptr), prev(nullptr),
+        constraints(_constraints) {}
+
+  Node(node_id_t _id, NodeType _type, const BDDNode_ptr &_next,
+       const BDDNode_ptr &_prev, klee::ConstraintManager _constraints)
+      : id(_id), type(_type), next(_next), prev(_prev),
+        constraints(_constraints) {}
+
+  void replace_next(const BDDNode_ptr &_next) {
+    assert(_next);
+    next = _next;
+  }
+
+  void add_next(const BDDNode_ptr &_next) {
+    assert(next == nullptr);
+    assert(_next);
+    next = _next;
+  }
+
+  void replace_prev(const BDDNode_ptr &_prev) {
+    assert(_prev);
+    prev = _prev;
+  }
+
+  void add_prev(const BDDNode_ptr &_prev) {
+    assert(prev == nullptr);
+    assert(_prev);
+    prev = _prev;
+  }
+
+  void disconnect() {
+    prev = nullptr;
+    next = nullptr;
+  }
+
+  const BDDNode_ptr &get_next() const { return next; }
+  const BDDNode_ptr &get_next() { return next; }
+
+  const BDDNode_ptr &get_prev() const { return prev; }
+  const BDDNode_ptr &get_prev() { return prev; }
+
+  NodeType get_type() const { return type; }
+  node_id_t get_id() const { return id; }
+
+  const klee::ConstraintManager &get_node_constraints() const {
+    return constraints;
+  }
+
+  klee::ConstraintManager get_constraints() const;
+
+  void set_constraints(const klee::ConstraintManager &_constraints) {
+    constraints = _constraints;
+  }
+
+  symbols_t get_generated_symbols() const;
+
+  // Get generated symbols, but no further than any of these node IDs
+  symbols_t get_generated_symbols(
+      const std::unordered_set<node_id_t> &furthest_back_nodes) const;
+
+  virtual symbols_t get_local_generated_symbols() const;
+
+  void update_id(node_id_t new_id);
+
+  static std::string process_call_path_filename(std::string call_path_filename);
+  void process_call_paths(std::vector<call_path_t *> call_paths);
+
+  virtual std::vector<node_id_t> get_terminating_node_ids() const {
+    if (!next) {
+      return std::vector<node_id_t>{id};
+    }
+
+    return next->get_terminating_node_ids();
+  }
+
+  virtual BDDNode_ptr clone(bool recursive = false) const = 0;
+  virtual void recursive_update_ids(node_id_t &new_id) = 0;
+  virtual void visit(BDDVisitor &visitor) const = 0;
+  virtual std::string dump(bool one_liner = false) const = 0;
+  virtual std::string dump_recursive(int lvl = 0) const;
+
+protected:
+  friend class Call;
+  friend class Branch;
+  friend class ReturnRaw;
+  friend class ReturnInit;
+  friend class ReturnProcess;
+
+  virtual std::string get_gv_name() const {
+    std::stringstream ss;
+    ss << id;
+    return ss.str();
+  }
+
+  friend class SymbolFactory;
+};
+} // namespace BDD
